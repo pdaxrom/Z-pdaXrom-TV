b85ae286354f039cbe7dee86362cae60b1fb27c7
pdaxrom <sashz@pdaxrom.org>
2018-04-27 11:40:52 +0800

    removed wrong release function

--- ft2232d-util/ft2232d-ctl.c
+++ ft2232d-util/ft2232d-ctl.c
@@ -12,20 +12,20 @@ struct usb_bus *USB_init()
     usb_init();
     usb_find_busses();
     usb_find_devices();
-    return(usb_get_busses());
+    return (usb_get_busses());
 }
 
 struct usb_device *USB_find(struct usb_bus *busses, struct usb_device *dev)
 {
     struct usb_bus *bus;
-    for(bus=busses; bus; bus=bus->next){
-	for(dev=bus->devices; dev; dev=dev->next) {
-	    if( (dev->descriptor.idVendor==USB_VENDOR) && (dev->descriptor.idProduct==USB_PRODUCT) ){
-		return( dev );
-	    }
-	}
+    for (bus = busses; bus; bus = bus->next) {
+        for (dev = bus->devices; dev; dev = dev->next) {
+            if ((dev->descriptor.idVendor == USB_VENDOR) && (dev->descriptor.idProduct == USB_PRODUCT)) {
+                return (dev);
+            }
+        }
     }
-    return( NULL );
+    return (NULL);
 }
 
 int main(int argc, char *argv[])
@@ -36,33 +36,29 @@ int main(int argc, char *argv[])
     printf("Find device...\n");
 
     bus = USB_init();
-    dev = USB_find(bus,dev);
-    
-    if (dev) {
-	struct usb_dev_handle *udev = NULL;
-	
-	printf("Open device...\n");
-	
-	udev = usb_open(dev);
-	if ((udev = usb_open(dev))){
-	    printf("Detach interface...\n");
-	    if (usb_detach_kernel_driver_np(udev, 0) < 0){
-		fprintf(stderr,"usb_set_configuration Error.\n");
-	        fprintf(stderr,"usb_detach_kernel_driver_np Error.(%s)\n", usb_strerror());
+    dev = USB_find(bus, dev);
 
-		if(usb_release_interface(udev, 0)){
-		    fprintf(stderr,"usb_release_interface() failed. (%s)\n",usb_strerror());
-		    usb_close(udev);
-		}
-		if( usb_close(udev)<0 ){
-		    fprintf(stderr,"usb_close Error.(%s)\n",usb_strerror());
-		}
-	    } else {
-		fprintf(stderr,"usb_open Error.(%s)\n", usb_strerror());
-	    }
-	}
+    if (dev) {
+        struct usb_dev_handle *udev = NULL;
+
+        printf("Open device...\n");
+
+        udev = usb_open(dev);
+        if ((udev = usb_open(dev))) {
+            printf("Detach interface...\n");
+            if (usb_detach_kernel_driver_np(udev, 0) < 0) {
+                fprintf(stderr, "usb_set_configuration Error.\n");
+                fprintf(stderr, "usb_detach_kernel_driver_np Error.(%s)\n", usb_strerror());
+            }
+
+            if (usb_close(udev) < 0) {
+                fprintf(stderr, "usb_close Error.(%s)\n", usb_strerror());
+            }
+        } else {
+            fprintf(stderr, "usb_open Error.(%s)\n", usb_strerror());
+        }
     } else {
-	fprintf(stderr, "device not found!\n");
+        fprintf(stderr, "device not found!\n");
     }
 
     return 0;
